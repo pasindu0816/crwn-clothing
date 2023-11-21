0093f231a0b5572ad22a02d7dff7d80ccb66c89e
pasindu0816 <pasinduratnayake.99@gmail.com>
2023-11-21 17:24:59 +0530

    added in our addToCart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,17 +7,16 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
-    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-
-    return (
-        <div className='cart-icon-container' onClick={toggleIsCartOpen}>
-            <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
-        </div>
-    );
-}; 
+  return (
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{cartCount}</span>
+    </div>
+  );
+};
 
 export default CartIcon;
